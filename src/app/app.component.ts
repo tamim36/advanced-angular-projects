484be689ae9499ffcc139329084d1659f14b0783
tamim <tamimarefinanik@gmail.com>
2025-01-14 13:14:05 +0600

    create configurable tasks component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './data/dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'easy-task-management';
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser(){
+    return this.users.find(user => user.id === this.selectedUserId)!;
+  }
 
   onUserSelect(id: string){
-    console.log(id);
+    this.selectedUserId = id;
   }
 }
