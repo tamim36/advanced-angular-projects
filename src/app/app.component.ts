e008070f14ecfdbe558b99bf78f2c37b2559f58c
tamim <tamimarefinanik@gmail.com>
2025-01-17 17:22:11 +0600

    preparing model to take user input and submit event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
+import { UserInputComponent } from "./user-input/user-input.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserInputComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { type UserInputModel } from './user-input.model';
+
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  @Output() userInputEvent = new EventEmitter<UserInputModel>();
+  initialInvestment = 0;
+  annualInvestment = 0;
+  expectedReturn = 0;
+  durationInMonth = 0;
+
+  onSubmit() {
+    const model: UserInputModel = {
+      initialInvestment: this.initialInvestment,
+      annualInvestment: this.annualInvestment,
+      expectedReturn: this.expectedReturn,
+      durationInMonth: this.durationInMonth
+    };
+
+    this.userInputEvent.emit(model);
+  }
 
 }
