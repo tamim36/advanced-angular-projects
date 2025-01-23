557b280f31a1abdf2c3cd6b31ab479c3a7ef3f0f
tamim <tamimarefinanik@gmail.com>
2025-01-23 20:36:52 +0600

    host binding to event and get host element info

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,10 +8,21 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)' : 'onClick()'
   }
 })
 export class ControlComponent {
   label = input.required<string>();
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick(){
+  //   console.log('cliecked');
+  // }
 
+  private hostElement = inject(ElementRef);
+
+  onClick (){
+    console.log('Cliced Success !');
+    console.log(this.hostElement);
+  }
 }
