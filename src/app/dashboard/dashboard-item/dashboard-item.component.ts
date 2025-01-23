26b5b36433672f3da3e4c672bbde4bde8919511e
tamim <tamimarefinanik@gmail.com>
2025-01-23 14:57:02 +0600

    using encapsulations and host for css scripting

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,11 +1,15 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
   standalone: true,
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'dashboard-item'
+  }
 })
 export class DashboardItemComponent {
   image = input.required<{ src: string, alt: string }>();
