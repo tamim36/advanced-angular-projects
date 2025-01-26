4ccd4242ccb646e804cfc126c3f74d6187fb89b3
tamim <tamimarefinanik@gmail.com>
2025-01-26 17:15:33 +0600

    build custom directive to use as confirmation when user click external link

---
+++ directive-deep-dive/src/app/safe-link.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, input } from "@angular/core";
+
+@Directive({
+    selector: 'a[appSafeLink]',
+    standalone: true,
+    host: {
+        '(click)' : 'onConfirmLeavePage($event)'
+    }
+})
+export class SafeLinkDirective {
+    queryParam = input('myApp');
+    constructor(){
+        console.log('Safe link directive is on action!');
+    }
+
+    onConfirmLeavePage(event : MouseEvent){
+        const isConfirm = window.confirm('Do you want to leave the app');
+
+        if (isConfirm){
+            const address = (event.target as HTMLAnchorElement).href;
+            (event.target as HTMLAnchorElement).href
+                = address + '?from=' + this.queryParam();
+                
+            return;
+        }
+
+        event.preventDefault();
+    }
+}
